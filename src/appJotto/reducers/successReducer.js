d8731d3246314a33a197b3d340dbd375e61cafe0
Pankaj Wakchaure <pankaj.corp@gmail.com>
2021-02-09 21:20:56 +0530

    redux setup completed

---
+++ src/appJotto/reducers/successReducer.js
@@ -0,0 +1,11 @@
+/**
+ * @function successReducer
+ * @param {array} state Array of guessed words
+ * @param {object} action action to be reduced
+ * @returns {boolean} new success state
+ */
+const successReducer = (state, action) => {
+  return null;
+};
+
+export default successReducer;
